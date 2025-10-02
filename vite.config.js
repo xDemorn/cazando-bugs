e92d2cb26f011c10c0d80a5447d3454df861ec16
Mikolaj Jaworski <jaworski.miki98@gmail.com>
2025-10-02 21:56:06 +0200

    added vite.config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// https://vite.dev/config/
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    base: '/cazando-bugs',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
+  appType: 'mpa'
+})
